239b1a1be4c54b7483d34e5f3abceb9dc2f9001c
lulianqi <mycllq@hotmail.com>
2024-02-27 02:24:23 +0800

    only test

--- Tests/TestConsoleDemo/Program.cs
+++ Tests/TestConsoleDemo/Program.cs
@@ -6,15 +6,17 @@ using DubboNet.DubboService.DataModle;
 using NetService.Telnet;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using TestConsoleDemo.DataModle;
 
 
 Console.WriteLine("TestDemoConsole");
-await TestForExTelnet();
-await TestForExTelnet();
-await TestForExTelnet();
+CasTest();
+//await TestForExTelnet();
+//await SendHttpRequestAsync(new Uri("http://baidu.com"));
 //await StressTestForDubboClient();
 //await TestForFinalize();
 Console.WriteLine("Enter to Exit");
@@ -25,6 +27,57 @@ for (int i = 0; i < 10; i++)
     GC.Collect(2, GCCollectionMode.Forced);
     GC.WaitForPendingFinalizers();
     Console.ReadLine();
+}
+
+static async Task SendHttpRequestAsync(Uri? uri = null, int port = 80, CancellationToken cancellationToken = default)
+{
+    uri ??= new Uri("http://example.com");
+
+    // Construct a minimalistic HTTP/1.1 request
+    byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.1
+Host: {uri.Host}
+Connection: Close
+
+");
+
+    // Create and connect a dual-stack socket
+    using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+    await socket.ConnectAsync(uri.Host, port, cancellationToken);
+
+    // Send the request.
+    // For the tiny amount of data in this example, the first call to SendAsync() will likely deliver the buffer completely,
+    // however this is not guaranteed to happen for larger real-life buffers.
+    // The best practice is to iterate until all the data is sent.
+    int bytesSent = 0;
+    while (bytesSent < requestBytes.Length)
+    {
+        bytesSent += await socket.SendAsync(requestBytes.AsMemory(bytesSent), SocketFlags.None);
+    }
+
+    // Do minimalistic buffering assuming ASCII response
+    byte[] responseBytes = new byte[256];
+    char[] responseChars = new char[256];
+
+    while (true)
+    {
+        int bytesReceived = await socket.ReceiveAsync(responseBytes, SocketFlags.None, cancellationToken);
+
+        // Receiving 0 bytes means EOF has been reached
+        if (bytesReceived == 0) break;
+
+        // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+        int charCount = Encoding.ASCII.GetChars(responseBytes, 0, bytesReceived, responseChars, 0);
+
+        // Print the contents of the 'responseChars' buffer to Console.Out
+        await Console.Out.WriteAsync(responseChars.AsMemory(0, charCount), cancellationToken);
+
+        Console.WriteLine("e to Exit");
+        if(Console.ReadLine()=="e")
+        {
+            break;
+        }
+    }
+    socket.Dispose();
 }
 
 static async Task TestForFinalize()
@@ -180,3 +233,16 @@ static async Task TestForSendQuery()
     Console.WriteLine(JsonSerializer.Serialize(DrModle_PlainResult.ResultModle, option));
 
 }
+
+static void CasTest()
+{
+    Cas cas = new Cas();
+    Cas cas1 = new Cas();
+    Cas cas2 = new Cas();
+}
+public class Cas
+{
+    static string Name = "1233";
+    public string ID = "";
+}
+
